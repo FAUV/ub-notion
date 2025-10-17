b11f372b4b823b7a1213e6aeea4ca288f86de5e6
Tester <test@example.com>
2025-10-17 15:38:44 -0300

    chore: improve capacitor server fallback

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,13 +3,19 @@ const config: CapacitorConfig = {
   appId: 'com.fauv.ubnotion',
   appName: 'ub-notion',
   webDir: 'out',
-  server: {
-    url: process.env.CAP_SERVER_URL || 'https://furcelay.com/ub-notion',
-    iosScheme: 'capacitor',
-    androidScheme: 'https',
-    cleartext: false,
-    hostname: 'ub-notion.local'
-  },
+  server: (() => {
+    const fallback =
+      process.env.NODE_ENV === 'production'
+        ? 'https://furcelay.com/ub-notion'
+        : 'http://localhost:3000';
+    return {
+      url: process.env.CAP_SERVER_URL || fallback,
+      iosScheme: 'capacitor',
+      androidScheme: 'https',
+      cleartext: false,
+      hostname: 'ub-notion.local'
+    };
+  })(),
   ios: { contentInset: 'automatic' }
 }
 export default config
